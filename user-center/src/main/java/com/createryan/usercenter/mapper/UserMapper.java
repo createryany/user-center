6c81b425d0aa08c93a85750b3e653f439c857257
createryan <9567228+createryan@user.noreply.gitee.com>
2022-08-03 16:50:28 +0800

    2.用户中心(规整项目)

--- user-center/src/main/java/com/createryan/usercenter/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.createryan.usercenter.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.createryan.usercenter.model.User;
-
-public interface UserMapper extends BaseMapper<User> {
-
-}
--- user-center/src/test/java/com/createryan/usercenter/UserCenterApplicationTests.java
+++ user-center/src/test/java/com/createryan/usercenter/UserCenterApplicationTests.java
@@ -1,29 +1,12 @@
 package com.createryan.usercenter;
 
-import com.createryan.usercenter.mapper.UserMapper;
-import com.createryan.usercenter.model.User;
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.annotation.Resource;
-import java.util.List;
 
 @SpringBootTest
 class UserCenterApplicationTests {
 
-    @Resource
-    private UserMapper userMapper;
-
     @Test
-    public void testSelect() {
-        System.out.println(("----- selectAll method test ------"));
-        List<User> userList = userMapper.selectList(null);
-        Assert.assertEquals(5, userList.size());
-        userList.forEach(System.out::println);
+    void testSelect() {
     }
-
-
 }
