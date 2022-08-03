19e758f71a83620d8b0b44699605d96d5507f2af
createryan <9567228+createryan@user.noreply.gitee.com>
2022-08-03 16:23:31 +0800

    2.用户中心(后端代码初始化)

---
+++ user-center/src/main/java/com/createryan/usercenter/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.createryan.usercenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.createryan.usercenter.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
